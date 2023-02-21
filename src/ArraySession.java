ca9a014f640b28c02b3c5a73a7e97ffa0bccaed4
kulwinder2510 <kulwinder9053@gmail.com>
2023-02-21 10:34:57 -0500

    array session nd method

---
+++ src/ArraySession.java
@@ -0,0 +1,81 @@
+ public class ArraySession {
+        public static void main(String[] args){
+            ArrayMethods arrayMethods = new ArrayMethods();
+            arrayMethods.whateverMethod();
+
+//            int[] numbers = new int[10];
+//
+//            numbers[0] = 100;
+//            numbers[1] = 101;
+//            numbers[2] = 102;
+//            numbers[3] = 103;
+//            numbers[4] = 104;
+//            numbers[5] = 105;
+//            numbers[6] = 106;
+//            numbers[7] = 107;
+//            numbers[8] = 108;
+//            numbers[9] = 109;
+
+            //System.out.println(numbers[5]);
+
+                                               /*or
+                                              can use for loop*/
+
+//            for (int i=0; i<=9; i++){
+//                System.out.println(numbers[i]);
+
+                                                  /*or
+                                                use counter as a variable*/
+
+//            int counter = 3500;
+//
+//            for (int i=0; i<=9; i++){
+//                numbers[i] = counter;
+//            }
+//            System.out.println(numbers[1]);
+
+
+                                                 /* or
+                                                 counter+any value
+                                                  */
+//            int counter = 3500;
+//
+//            for (int i=0; i<=9; i++){
+//                numbers[i] = counter+5;
+//            }
+//            System.out.println(numbers[7]);
+
+
+                                                   /* or
+                                                   put counter value
+                                                   nd use i--
+                                                    */
+
+
+
+//            for (int i = 0; i >= 0; i--) {
+//                if (numbers[i] == 104) {
+//                    ;
+//                }
+//                System.out.println(numbers[i]);
+
+
+            //or
+
+//            int counter = 3500;
+//
+//            for (int i=0; i<=9; i++){
+//                numbers[i] = counter+5;
+//            }
+//            System.out.println(numbers[7]);
+
+
+            //or
+
+
+
+            }
+
+        }
+
+
